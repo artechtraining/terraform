0ea8543231f7ae97982a08eee5eb622230a671a6
artechtraining <167943955+artechtraining@users.noreply.github.com>
2024-06-21 09:23:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+	region = "us-east-2a"
+	access_key = "AKIA6GBMARVOFKVVDLHD"
+	secret_key     = "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"
+}
 resource "aws_instance" "web" {
   ami           = "ami-05a36e1502605b4aa"
   instance_type = "t2.nano"
